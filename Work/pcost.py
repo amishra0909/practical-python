d685b35fa157ef97459b13679e7e70046e8c2ac4
Abhishek Mishra <abhiusedu@gmail.com>
2021-12-22 00:54:32 -0800

    Exercise 3.14: Use report library function in pcost.py.

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,29 +2,39 @@
 #
 # Exercise 1.27
 
+from report import read_portfolio
 import csv
 import sys
 
+
 def portfolio_cost(filename):
-    '''
+    """
     This takes a portfolio file and outputs the value of total cost.
-    '''
+    """
     total = 0.0
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        header = next(rows)
-        for i, row in enumerate(rows, start = 1):
-            record = dict(zip(header, row))
-            try:
-                name, shares, price = (record['name'], int(record['shares']), float(record['price']))
-                total += shares * price
-            except:
-                print(f'Row {i:>2}: Couldn\'t convert: {row}')
+
+    portfolio = read_portfolio(filename)
+    for i, row in enumerate(portfolio, start=1):
+        try:
+            total += row['shares'] * row['price']
+        except Exception:
+            print(f'Row {i:>2}: Could not convert: {row}')
+    # with open(filename, 'rt') as f:
+    #     rows = csv.reader(f)
+    #     header = next(rows)
+    #     for i, row in enumerate(rows, start=1):
+    #         record = dict(zip(header, row))
+    #         try:
+    #             name, shares, price = (record['name'], int(record['shares']), float(record['price']))
+    #             total += shares * price
+    #         except Exception:
+    #             print(f'Row {i:>2}: Could not convert: {row}')
     return total
 
-filename = 'Data/portfolio.csv'
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
 
-cost = portfolio_cost(filename)
+filepath = 'Data/portfolio.csv'
+if len(sys.argv) == 2:
+    filepath = sys.argv[1]
+
+cost = portfolio_cost(filepath)
 print('Total cost:', cost)
